e05863195d5a93b63492cf8f2c7da9890c98e1ee
Austin <austinaryain@users.noreply.github.com>
2021-02-02 14:19:04 -0600

    remove test data

--- app/src/main/java/com/austinaryain/fetchchallenge/data/remote/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/austinaryain/fetchchallenge/data/remote/RemoteDataSourceImpl.kt
@@ -11,35 +11,6 @@ class RemoteDataSourceImpl @Inject constructor(val fetchClient: FetchClient) :
         return try {
             Either.Right(
                 fetchClient.getData()
-//                listOf(
-//                    FetchItem(1, 1, "Test"),
-//                    FetchItem(2, 2, "Test"),
-//                    FetchItem(3, 12, null),
-//                    FetchItem(4, 926, "Test"),
-//                    FetchItem(5, 10, "Test"),
-//                    FetchItem(6, 111, "Test"),
-//                    FetchItem(7, 12, "Test"),
-//                    FetchItem(8, 12, "Test"),
-//                    FetchItem(9, 101, "Test"),
-//                    FetchItem(1, 1, "Test"),
-//                    FetchItem(2, 2, "Test"),
-//                    FetchItem(3, 12, null),
-//                    FetchItem(4, 926, "Example"),
-//                    FetchItem(5, 10, "Omega"),
-//                    FetchItem(6, 111, "Zeta"),
-//                    FetchItem(7, 12, "Start"),
-//                    FetchItem(8, 12, "Beta"),
-//                    FetchItem(9, 101, "Alpha"),
-//                    FetchItem(1, 1, "Test"),
-//                    FetchItem(2, 2, "Test"),
-//                    FetchItem(3, 12, null),
-//                    FetchItem(4, 926, "Charlie"),
-//                    FetchItem(5, 10, "Echo"),
-//                    FetchItem(6, 111, "Bravo"),
-//                    FetchItem(7, 12, "Delta"),
-//                    FetchItem(8, 12, "Why"),
-//                    FetchItem(9, 101, "Test"),
-//                )
             )
         } catch (ex: Exception) {
             Either.Left(ex)
